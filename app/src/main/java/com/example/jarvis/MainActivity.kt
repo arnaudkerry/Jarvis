15199492c4757b64a991d5b01fe5c315bc2c4943
arnaudkerry <45394459+arnaudkerry@users.noreply.github.com>
2020-09-09 15:09:18 +0200

    Swipe Listener sur Jarvis (a corriger)

--- app/src/main/java/com/example/jarvis/MainActivity.kt
+++ app/src/main/java/com/example/jarvis/MainActivity.kt
@@ -8,18 +8,33 @@ import android.view.Window
 import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.TextView
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navigation_view = findViewById(R.id.navigation_view) as View
         val new_button = findViewById(R.id.newThing) as ImageView
         val new_commande = findViewById(R.id.new_commande) as TextView
         val new_reunion = findViewById(R.id.new_reunion) as TextView
         val accueil = findViewById(R.id.accueil) as View
+        jarvis_view.setOnTouchListener(object: OnSwipeTouchListener(this@MainActivity) {
+            override fun onSwipeRight() {
+                navigation_view.visibility = View.VISIBLE
+            }
 
+            override fun onSwipeLeft() {
+                navigation_view.visibility = View.INVISIBLE
+            }
+        })
         new_button.setOnClickListener {
             new_commande.visibility = View.VISIBLE
             new_reunion.visibility = View.VISIBLE
@@ -34,7 +49,11 @@ class MainActivity : AppCompatActivity() {
                 new_commande.visibility = View.INVISIBLE
                 new_reunion.visibility = View.INVISIBLE
             }
+
+            if(navigation_view.visibility == View.VISIBLE) navigation_view.visibility = View.INVISIBLE
         }
+
+
     }
 
 
